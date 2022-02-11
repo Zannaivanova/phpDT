23533225fadb26e0c9ed8625864ba10732a894f9
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-11 16:10:52 +0200

    pp.Pm.Ar.Chn

--- code.php
+++ code.php
@@ -1,17 +1,28 @@
 <?php 
 //Задача №1
-$arr = ['a', 'b', 'c', 'd', 'e'];
-echo count($arr) . '<br>';
+$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+$arr['a'] = 5;
+$arr['b'] = '$';
+$arr['c'] = '@';
+
+var_dump($arr) . '<br>';
 
 //Задача №2
-$arr = ['a'=>1, 'b'=> 2, 'c'=>3, 'd'=>4];
-echo count($arr) . '<br>';
+$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+$arr['a'] = 3;
+$arr['b'] = 3;
+$arr['c'] = 3;
+
+var_dump($arr) . '<br>';
 
 //Задача №3
-$arr = ['a', 'b', 'c', 'd', 'e'];
-echo ($arr[count($arr)-1]) . '<br>';
+$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+$arr['a']++;
+$arr['a']++;//3
+$arr['b']--;//1
+$arr['c']--;//
+$arr['c']--;//1
 
-//Задача №4
-$arr = ['a', 'b', 'c', 'd', 'e'];
-echo ($arr[count($arr)-2]) . '<br>';
+var_dump($arr) . '<br>';
+
 ?>
