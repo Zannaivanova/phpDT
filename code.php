ec55c2f0960edabb9d88a26a26131b577baa57a3
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-11 13:40:10 +0200

    pp.Pm.Ar.Inr

--- code.php
+++ code.php
@@ -1,68 +1,32 @@
 <?php 
 //Задача №1
-$num = 10;
-++$num;
-++$num;
---$num;
+$arr = ['a', 'b', 'c'];
+echo $arr . '<br>';//Warning: Array to string conversion in /home/n/Документы/123/phpDT/code.php on line 5
+var_dump($arr);
 
-echo $num . '<br>';//11
+//Задача №1
+$arr = array('1','2','3');
+foreach ($arr as $value) {
+    echo $value, "\n";
+}
+var_dump($arr);
 
 //Задача №2
-$num = 3;
+$arr = ['a','b','c'];
 
-echo ++$num . '<br>';//4
+echo $arr[0];
+echo $arr[1];
+echo $arr[2]. '<br>';
+
 
 //Задача №3
-$num = 3;
+$arr = ['a','b','c','d'];
 
-echo $num++ . '<br>';//3
-
-
-//Задача №4
-$num = 3;
-
-echo --$num . '<br>';//2
-
-//Задача №5
-$num = 3;
-
-echo $num-- . '<br>';//3
-
-//Задача №6
-$num1 = 3;
-$num2 = ++$num1;
-
-echo $num1 . '<br>';//4
-echo $num2 . '<br>';//4
+echo $arr[0] . '+'. $arr[1] . '+'. $arr[2] . '+'. $arr[3] . '<br>';
 
 
-//Задача №7
-$num1 = 3;
-$num2 = $num1++;
+//Задача №4s
+$arr = [1,2,3];
 
-echo $num1 . '<br>';//4
-echo $num2 . '<br>';//3
-
-//Задача №8
-$num1 = 3;
-$num2 = --$num1;
-
-echo $num1 . '<br>';//2
-echo $num2 . '<br>';//2
-
-//Задача №9
-$num1 = 3;
-$num2 = $num1--;
-
-echo $num1 . '<br>';//2
-echo $num2 . '<br>';//3
-
-
-//Задача №10
-$num1 = 3;
-$num1++;
-$num2 = $num1--;
-
-echo $num1++ . '<br>';//3
-echo --$num2 . '<br>';//3
+echo ($arr[0]+$arr[1]+$arr[2]) . '<br>';
 ?>
