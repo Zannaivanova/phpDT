a146065337aa0d0a2915f9eb1c8de6f047b543db
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-11 08:55:00 +0200

    pp.Pm.Bs.SCh

--- code.php
+++ code.php
@@ -1,22 +1,72 @@
 <?php 
 //Задача №1
-$test ='12345';
-var_dump ((int)($test)) . '<br>';
+$str ='abcde';
+
+echo $str[0] . '<br>';
+echo $str[2] . '<br>';
+echo $str[4]. '<br>'. '<br>';
 
 //Задача №2
-$test ='12.345';
-var_dump ((float)($test)) . '<br>';
+$str ='abcde';
+
+echo $str[4];
+echo $str[3];
+echo $str[2];
+echo $str[1];
+echo $str[0]. '<br>'. '<br>';
+
 
 //Задача №3
-$test ='12.345';
-var_dump ((int)($test)) . '<br>';
+$str ='abcde';
+$num = 0;
+
+echo $str[$num]  . '<br>'. '<br>'. '<br>';
+
 
 //Задача №4
-$test = 12345;
-var_dump ((string)($test)) . '<br>';
+$str ='qwerty';
+$last = strlen($str) - 1;
 
+echo $str[$last] . '<br>';
 
 //Задача №5
-$test = 12.345;
-var_dump ((string)($test)) . '<br>';
+$str ='qwerty';
+$last = strlen($str) - 2;
+
+echo $str[$last] . '<br>';
+
+//Задача №6
+$str ='qwerty';
+$last = strlen($str) - 3;
+
+echo $str[$last]  . '<br>'. '<br>'. '<br>';
+
+
+//Задача №7
+$str ='12345';
+
+echo $str[0]+ $str[1]+ $str[2]+ $str[3]+ $str[4] . '<br>'. '<br>'. '<br>';
+
+//Задача №8
+$num = 12345;
+$str = (string)$num;
+
+echo $str[0]+ $str[1]+ $str[2]+ $str[3]+ $str[4] . '<br>';
+
+//Задача №9
+$num = 12345;
+$str = (string)$num;
+
+echo $str[0]* $str[1]* $str[2]* $str[3]* $str[4] . '<br>';
+
+//Задача №10
+$num = 12345;
+$str = (string)$num;
+	
+	echo $str[4];
+	echo $str[3];
+	echo $str[2];
+	echo $str[1];
+	echo $str[0];
+
 ?>
