92e05c8f289aec0719a15a906cb42021e06b3d04
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-11 09:35:07 +0200

    pp.Pm.Bs.ShO

--- code.php
+++ code.php
@@ -1,72 +1,33 @@
 <?php 
 //Задача №1
-$str ='abcde';
+$num = 1;
+$num = $num + 1;
+$num = $num + 1;
 
-echo $str[0] . '<br>';
-echo $str[2] . '<br>';
-echo $str[4]. '<br>'. '<br>';
+echo $num . '<br>';//3
 
 //Задача №2
-$str ='abcde';
+$num = 1;
+$num = $num + 2;
+$num = $num + 3;
 
-echo $str[4];
-echo $str[3];
-echo $str[2];
-echo $str[1];
-echo $str[0]. '<br>'. '<br>';
+echo $num . '<br>';//6
 
 
 //Задача №3
-$str ='abcde';
-$num = 0;
+$num = 47;
+$num += 7;
+$num -= 18;
+$num *= 10;
+$num /= 15;
 
-echo $str[$num]  . '<br>'. '<br>'. '<br>';
-
+echo $num . '<br>';
 
 //Задача №4
-$str ='qwerty';
-$last = strlen($str) - 1;
-
-echo $str[$last] . '<br>';
-
-//Задача №5
-$str ='qwerty';
-$last = strlen($str) - 2;
-
-echo $str[$last] . '<br>';
-
-//Задача №6
-$str ='qwerty';
-$last = strlen($str) - 3;
-
-echo $str[$last]  . '<br>'. '<br>'. '<br>';
+$str = 'a';
+$str .= 'b';
+$str .= 'c';
 
 
-//Задача №7
-$str ='12345';
-
-echo $str[0]+ $str[1]+ $str[2]+ $str[3]+ $str[4] . '<br>'. '<br>'. '<br>';
-
-//Задача №8
-$num = 12345;
-$str = (string)$num;
-
-echo $str[0]+ $str[1]+ $str[2]+ $str[3]+ $str[4] . '<br>';
-
-//Задача №9
-$num = 12345;
-$str = (string)$num;
-
-echo $str[0]* $str[1]* $str[2]* $str[3]* $str[4] . '<br>';
-
-//Задача №10
-$num = 12345;
-$str = (string)$num;
-	
-	echo $str[4];
-	echo $str[3];
-	echo $str[2];
-	echo $str[1];
-	echo $str[0];
-
+echo $str . '<br>';
 ?>
