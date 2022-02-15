e0cc40eb13f422c74b2b762846c127c9461ba2bd
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-15 09:15:01 +0200

    pp.Pm.Ar.VK

--- code.php
+++ code.php
@@ -1,27 +1,14 @@
 <?php 
 //Задача №1
-$arr = [];
-$arr[] = '1';
-$arr[] = '2';
-$arr[] = '3';
-$arr[] = '4';
-$arr[] = '5';
+$arr = [1, 2, 3, 4, 5];
+$key1 = 1;
+$key2 = 2;
 
-var_dump($arr) . '<br>';
+echo ($arr[$key1]+$arr[$key2]) . '<br>';
 
 //Задача №2
-$arr = [1, 2, 3];
+$arr = ['a'=>1, 'b'=> 2, 'c'=> 3];
+$key = 'b';
 
-$arr[] = 4;
-$arr[] = 5;
-
-var_dump($arr) . '<br>';
-
-//Задача №3
-$arr= [];
-$arr['year']='2022';
-$arr['month']='february';
-$arr['day']='11';
-
-var_dump($arr) . '<br>';
+echo $arr[$key];
 ?>
