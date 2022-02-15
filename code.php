848b8d12ecf649d5d2c78b2c5dbc66be922e0324
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-15 11:06:43 +0200

    pp.Pm.Cd.CS

--- code.php
+++ code.php
@@ -1,111 +1,147 @@
 <?php 
 //Задача №1
-$test1 = '3';
-$test2 = '3';
-
-if ($test1 == $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//+
+ $num = 2;
+	
+ if ($num > 0 and $num < 5) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';
 
 //Задача №2
-$test1 = '3';
-$test2 = '3';
+ $num = 2;
+	
+ if ($num >= 10 and $num <=20) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';
 
-if ($test1 === $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//+
+ //Задача №3
+ $num1 = 3;
+ $num2 = 2;
 
-//Задача №3
-$test1 = 3;
-$test2 = '3';
+ if ($num1 <= 1 and $num2 >=3) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';
 
-if ($test1 == $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//+
+ //Задача №4
+ $num1 = -10;
+ $num2 = -10;
 
-//Задача №4
-$test1 = 3;
-$test2 = '3';
+ if ($num1 <= 0 or $num2 >=0) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-if ($test1 === $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//-
+ //Задача №5
+ $num1 = 0;
+ $num2 = 0;
 
-//Задача №5
-$test1 = 3;
-$test2 = 3;
+ if ($num1 <= 0 or $num2 >=0) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-if ($test1 === $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//+
+  //Задача №6
+ $num1 = 0;
+ $num2 = 5;
 
-//Задача №6
-$test1 = '3';
-$test2 = 3;
+ if ($num1 <= 0 or $num2 >=0) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-if ($test1 != $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//-
+  //Задача №7
+ $num1 = 5;
+ $num2 = 5;
 
-//Задача №7
-$test1 = '3';
-$test2 = '3';
+ if ($num1 <= 0 or $num2 >=0) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-if ($test1 !== $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//-
 
-//Задача №8
-$test1 = 3;
-$test2 = '3';
+  //Задача №8
+ $num1 = -5;
+ $num2 = 15;
 
-if ($test1 != $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//-
+ if ($num1 <= 0 or $num2 >=0) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-//Задача №9
-$test1 = 3;
-$test2 = '3';
+ //Задача №9
+ $num = 1;
+	
+ if ($num == 0 or $num ==1) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-if ($test1 !== $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//+
+ //Задача №10
+ $num = 1;
+	
+ if ($num == 0 or $num ==2) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//-
 
-//Задача №10
-$test1 = 3;
-$test2 = 2;
+//Задача №11
+ $num = 2;
+	
+ if ($num == 0 or $num ==1 or $num ==2) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';//+
 
-if ($test1 !== $test2) {
-    echo '+';
-} else {
-	echo '-';
-}
-echo '<br>';//+
+//Задача №12
+ $num = 3;
+	
+ if (($num > 5 and $num < 10) or $num == 20) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';
+
+//Задача №13
+ $num = 3;
+	
+ if ($num > 5 or ($num > 10 and $num < 3)) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+ echo '<br>';
+
+ //Задача №14
+ $num = 3;
+	
+ if ($num == 9 or ($num > 10 and $num < 20) or ($num > 20 and $num < 30)) {
+		echo '+';
+	} else {
+		echo '-';
+	}
 ?>
