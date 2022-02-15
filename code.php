9d4dfaa29cc360ccf27c8b6a25c099b7a828f75c
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-15 14:07:14 +0200

    pp.Pm.Cd.BT

--- code.php
+++ code.php
@@ -1,9 +1,219 @@
 <?php 
 //Задача №1
- if (!($num1 >= 0 and $num2 <= 10)) {
+$test = true;
+
+ if ($test === true) {
 		echo '+';
 	} else {
 		echo '-';
 	}
+echo '<br>';
+
+//Задача №2
+$test = true;
+
+ if ($test === false) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';
+
+//Задача №3
+$test = 1;
+
+ if ($test == true) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//+
+
+//Задача №4
+$test = 0;
+
+ if ($test == true) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №5
+$test = 1;
+
+ if ($test == false) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №6
+$test = 1;
+
+ if ($test != true) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №7
+$test = '';
+
+ if ($test == false) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//+
+
+//Задача №8
+//test;
+
+ //if (test == true) {
+//		echo '+';
+//	} else {
+//		echo '-';}
+//echo '<br>';//¯\_(ツ)_/¯ warning/notice это ж не переменная, вроде
+
+//Задача №9
+$test = true;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';
+
+//Задача №10
+$test = true;
+
+ if (!$test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';
+
+//Задача №11
+$test = true;
+
+ if (!$test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';
+
+//Задача №12
+$test = true;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';
+
+//Задача №13
+$test = 3;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//+
+
+//Задача №14
+$test = 'abc';
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//+
+
+//Задача №15
+$test = '';
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №16
+//$test = 3 * 'abc';
+
+// if ($test) {
+//		echo '+';
+//	} else {
+//		echo '-';}
+//echo '<br>';//- (ой)
+
+//Задача №17
+$test = null;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №18
+$test = false;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №19
+$test;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №20
+$test = 0;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №21
+$test = '0';
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}
+echo '<br>';//-
+
+//Задача №22
+$test = -1;
+
+ if ($test) {
+		echo '+';
+	} else {
+		echo '-';
+	}//- (ой)
 
 ?>
