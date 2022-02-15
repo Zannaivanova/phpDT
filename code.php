087fc72b97878e031276d0dfe19f33f8120e2f99
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-15 10:23:15 +0200

    pp.Pm.Cd.DT

--- code.php
+++ code.php
@@ -1,73 +1,111 @@
 <?php 
 //Задача №1
-$test = 5;
+$test1 = '3';
+$test2 = '3';
 
-if ($test > 10) {
+if ($test1 == $test2) {
     echo '+';
 } else {
 	echo '-';
 }
-echo '<br>';
-
+echo '<br>';//+
 
 //Задача №2
-$test = 5;
+$test1 = '3';
+$test2 = '3';
 
-if ($test < 10) {
+if ($test1 === $test2) {
     echo '+';
 } else {
 	echo '-';
 }
-echo '<br>';
+echo '<br>';//+
 
 //Задача №3
-$test = 10;
+$test1 = 3;
+$test2 = '3';
 
-if ($test <= 10) {
+if ($test1 == $test2) {
     echo '+';
 } else {
 	echo '-';
 }
-echo '<br>';
+echo '<br>';//+
 
 //Задача №4
-$test = 10;
+$test1 = 3;
+$test2 = '3';
 
-if ($test >= 10) {
+if ($test1 === $test2) {
     echo '+';
 } else {
 	echo '-';
 }
-echo '<br>';
+echo '<br>';//-
 
 //Задача №5
-$test = 10;
+$test1 = 3;
+$test2 = 3;
 
-if ($test == 10) {
+if ($test1 === $test2) {
     echo '+';
 } else {
 	echo '-';
 }
-echo '<br>';
-
+echo '<br>';//+
 
 //Задача №6
-$test = 10;
+$test1 = '3';
+$test2 = 3;
 
-if ($test != 10) {
+if ($test1 != $test2) {
     echo '+';
 } else {
 	echo '-';
 }
-echo '<br>';
+echo '<br>';//-
 
 //Задача №7
-$test1 = 5;
-$test2 = 10;
+$test1 = '3';
+$test2 = '3';
 
-if ($test1 > $test2) {
+if ($test1 !== $test2) {
     echo '+';
 } else {
 	echo '-';
 }
+echo '<br>';//-
+
+//Задача №8
+$test1 = 3;
+$test2 = '3';
+
+if ($test1 != $test2) {
+    echo '+';
+} else {
+	echo '-';
+}
+echo '<br>';//-
+
+//Задача №9
+$test1 = 3;
+$test2 = '3';
+
+if ($test1 !== $test2) {
+    echo '+';
+} else {
+	echo '-';
+}
+echo '<br>';//+
+
+//Задача №10
+$test1 = 3;
+$test2 = 2;
+
+if ($test1 !== $test2) {
+    echo '+';
+} else {
+	echo '-';
+}
+echo '<br>';//+
 ?>
