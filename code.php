377f047846a9033e21d07d61ca1f557d67bec46a
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-15 17:16:30 +0200

    pp.Pm.Cd.ShS

--- code.php
+++ code.php
@@ -1,141 +1,20 @@
 <?php 
 //Задача №1
-$test = 0;
-
- if (empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
+if ($test = 10) {
+		echo 'верно';
+	} 
+echo '<br>';
 
 //Задача №2
-$test = -1;
-
- if (empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//-
+if ($test == 0) 
+	echo 'верно';
+ else
+	echo 'неверно'; 
+echo '<br>';
 
 //Задача №3
-$test = '';
+if ($test == 0) 
+	echo 'верно';
 
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//-
 
-//Задача №4
-$test = -1;
-
- if (empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//-
-
-//Задача №5
-$test = '0';
-
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//-
-
-//Задача №6
-$test = -1;
-
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
-
-//Задача №7
-$test = null;
-
- if (empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
-
-//Задача №8
-$test = false;
-
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//-
-
-//Задача №9
-$test = true;
-
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
-
-//Задача №10
-$test = 'false';
-
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
-
-//Задача №11
-$test = 'null';
-
- if (!empty($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
-
-//Задача №12
-$test = 0;
-
- if (isset($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
-
-//Задача №13
-$test = null;
-
- if (isset($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//-
-
-//Задача №14
-$test = false;
-
- if (isset($test)) {
-		echo '+';
-	} else {
-		echo '-';
-	}
-echo '<br>';//+
 ?>
