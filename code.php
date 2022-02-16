81c286c2a6afb6e907973a59c942e0536e55114d
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-16 09:12:40 +0200

    pp.Pm.Cd.TO

--- code.php
+++ code.php
@@ -1,21 +1,7 @@
 <?php 
 //Задача №1
-switch ($num=10) {
-		case ($num>=1 and $num<=2)or $num==12:
-			echo 'winter';
-		break;
-		case $num>=3 and $num<=5:
-			echo 'spring';
-		break;
-		case $num>=6 and $num<=8:
-			echo 'summer';
-		break;
-		case $num>=9 and $num<=11:
-			echo 'autumn';
-		break;
-		default:
-			echo 'нет такого сезона';
-		break;
-	}
+	$num = -1;
+	$result = $num > 0 ? print ('положительное'): print ('отрицательное');
+
 echo '<br>';
 ?>
