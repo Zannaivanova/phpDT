24be66b96cf44449db755a36ded290ef67fdaef0
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-16 17:35:23 +0200

    pp.Pm.Lp.WM

--- code.php
+++ code.php
@@ -1,46 +1,45 @@
 <?php
 // Задача 1
-$i = 1;
-
-while($i<=100){
-	echo $i . "\n";
-	$i++;
-} 
+$i = 10;
+	
+while ($i >= 1) {
+		echo $i;
+		$i--;
+}
 echo '<br>';
 
 // Задача 2
-$i = 11;
-
-while($i<=33){
-	echo $i . "\n";
-	$i++;
-} 
+$i = 10;
+	
+	while ($i >= 1) {
+		echo $i;
+		$i--;
+	}
 echo '<br>';
 
 // Задача 3
-$i = 0;
-
-while($i<=100){
-	echo $i . "\n";
-	$i +=2;
-} 
+$i = 10;
+	
+	while ($i > 0) {
+		echo $i;
+		$i--;
+	}
 echo '<br>';
 
 // Задача 4
-$i = 1;
-
-while($i<=99){
-	echo $i . "\n";
-	$i +=2;
-} 
+	$i = 10;
+	
+	while ($i > 0) {
+		echo $i;
+		$i--;
+	}
 echo '<br>';
 
 // Задача 5
-$i = 30;
-
-while($i>=0){
-	echo $i . "\n";
-	$i--;
-} 
-
+	$i = 10;
+	
+	while ($i > 0) {
+		echo $i;
+		$i--;
+	}
 ?>
