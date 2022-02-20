21002710cd81d0467453548968928a430e82b370
Zannaivanova <Zannaivanova@tutanota.com>
2022-02-20 16:35:24 +0200

    pp.Pm.Lp.RLA

--- code.php
+++ code.php
@@ -1,33 +1,33 @@
 <?php
 // Задача 1
-for ($i = 0; $i<=100; $i++) {
-		echo $i."\n";
+$result = 1;
+
+for ($i = 1; $i<=20; $i++) {
+
+		$result = $result * $i;
 }
+echo $result;
+
 echo '<br>';
 
 // Задача 2
-for ($i = 11; $i<=33; $i++) {
-		echo $i."\n";
+$result = 0;
+
+for ($i = 2; $i<=100; ) {
+        $i+=2;
+		$result = $result + $i;
 }
+echo $result;
+
 echo '<br>';
 
 // Задача 3
-for ($i = 0; $i<=100;) {
-		echo $i."\n";
-		$i +=2;
-}
-echo '<br>';
+$result = 0;
 
-// Задача 4
-for ($i = 1; $i<=99;) {
-		echo $i."\n";
-		$i +=2;
+for ($i = 1; $i<=99; ) {
+        $i+=2;
+		$result = $result + $i;
 }
-echo '<br>';
-
-// Задача 5
-for ($i = 100; $i>=0; $i--) {
-		echo $i."\n";
-}
+echo $result;
 
 ?>
